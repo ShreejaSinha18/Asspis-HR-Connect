709b4544d97bb35f32e58f7902ea6f9f32444775
ShreejaSinha18 <shreejasinha05@gmail.com>
2024-09-14 16:49:43 +0530

    feat: header of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,5 +3,18 @@
 </script>
 
 <template>
-    <p>Hello World!!</p>
+    <div id="head">
+        <h1>ASSPIS HR CONNECT</h1>
+        <div class="menu">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Pricing</a>
+            <a href="#">About Us</a>
+            <a href="#">Testimonials</a>
+            <a href="#">Contact</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+        </div>
+        
+    </div>
 </template>
