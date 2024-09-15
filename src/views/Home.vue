da838309534ceaff7f88746ab97af8ccb7077580
ShreejaSinha18 <shreejasinha05@gmail.com>
2024-09-15 22:50:19 +0530

    feat: update body of home and add images.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,48 @@
             <a href="#">Privacy Policy</a>
             <a href="#">Terms & Conditions</a>
         </div>
+    </div>
+    <div id="body">
+        <div class="right">
+            <img style="border-radius: 10px; width: 300px; height: 250px;" src="../assets/hr.jpg">
+        </div>
+        <div style="margin-top: 40px;" class="container1">
+            <h3>Welcome to ASSPIS HR Connect!!</h3>
+            <p>
+                In today’s fast-paced business environment, efficiency and adaptability are key to staying ahead. At ASSPIS, we are committed to transforming the way organizations operate by integrating cutting-edge AI-driven chatbot technology into your enterprise systems.
+            </p>
+            <p>
+                Our intelligent chatbot is designed to enhance organizational efficiency, streamline operations, and improve overall productivity. By automating routine tasks and providing instant, accurate responses, our solution allows your team to focus on what truly matters—driving growth and innovation.
+            </p>
+        </div>
         
+        <div class="container2">
+            <h3 style="text-align: center; font-weight: bolder;">Gallery</h3>
+            <div class="home-pic">
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home1.jpg">
+                </div>
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home2.webp">
+                </div>
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home3.jpeg">
+                </div>
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home4.jpg">
+                </div>
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home5.webp">
+                </div>
+                <div class="col">
+                    <img style="height: 250px; width: 400px;" src="../assets/home6.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="margin-top: 40px;" id="foot">
+        <div style="quicklinks">
+            
+        </div>
     </div>
 </template>
